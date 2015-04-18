432ec7bb9cd4691f04d342b63d97729a25c8fd9a
Will McCabe <wmccabe@csumb.edu>
2015-04-18 00:55:32 +0000

    Fixed registration form to add new forms (FirstName, LastName, CurrentLocation).

--- Travelers App/app/views/register.js
+++ Travelers App/app/views/register.js
@@ -4,19 +4,25 @@ var frameModule = require("ui/frame");
 var images = require("../shared/utils/images");
 var pageData = require("../shared/models/userCredentials");
 var viewModule = require("ui/core/view");
-
+var scrollViewModule = require("ui/scroll-view");
 
 exports.load = function (args) {
+    var scrollView = new scrollViewModule.ScrollView();
+
     var page = args.object;
     var email = viewModule.getViewById(page, "email");
     var username = viewModule.getViewById(page, "username");
+    var firstname = viewModule.getViewById(page, "firstname");
+    var lastname = viewModule.getViewById(page, "lastname");
 
     pageData.set("email_address", "");
     pageData.set("username", "");
     pageData.set("password", "");
     pageData.set("logoSource", images.logo);
-    page.bindingContext = pageData;
+    pageData.set("firstName", "");
+    pageData.set("lastName", "");
 
+    page.bindingContext = pageData;
     // Turn off autocorrect and autocapitalization for iOS
     if (username.ios) {
         email.ios.autocapitalizationType =
@@ -30,8 +36,14 @@ exports.load = function (args) {
     }
 };
 exports.register = function () {
+    var currentLoc = "DEFAULT";
+    
+    //Adds username, password, Email, FirstName, LastName, and CurrentLocation
     el.Users.register(pageData.get("username"), pageData.get("password"), {
-        Email: pageData.get("email_address")
+        Email: pageData.get("email_address"),
+        FirstName: pageData.get("firstName"),
+        LastName: pageData.get("lastName"),
+        CurrentLocation: currentLoc
     }, function (response) {
         dialogs.alert("Your account was successfully created.").then(function () {
             frameModule.topmost().navigate("app/views/login");
