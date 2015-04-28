13c745cde42357bb70b1af7adbc021dcfcd3b772
Will McCabe <wmccabe@csumb.edu>
2015-04-28 21:06:41 +0000

    added chat button

--- Travelers App/app/views/userPage.js
+++ Travelers App/app/views/userPage.js
@@ -4,6 +4,10 @@ var frameModule = require("ui/frame");
 var images = require("../shared/utils/images");
 var pageData = require("../shared/models/userCredentials");
 var viewModule = require("ui/core/view");
+var webViewModule = require("ui/web-view");
+
+//Create a webView to open a web page
+var webView = new webViewModule.WebView();
 
 
 exports.load = function (args) {
@@ -12,8 +16,14 @@ exports.load = function (args) {
 };
 
 
-// Adds merchant list here on button tap
+// Function that navigates to list of merchants (merchantList) when button is tapped
 exports.toMerchantList = function (args) {
     var topmost = frameModule.topmost();
     topmost.navigate("app/views/merchantList");
+};
+
+// Adds merchant list here on button tap
+exports.toChat = function (args) {
+    var topmost = frameModule.topmost();
+    topmost.navigate("app/views/chatPage");
 };
